fa6fdd7a869daf7dfba296b15f4aab2f168aed96
Daniel Kent <Kentpercussion@gmail.com>
2019-10-21 13:33:16 -0600

    fixed error

--- drills.js
+++ drills.js
@@ -1,5 +1,9 @@
 'use strict';
 function createGreeting(name, age) {
+  if (age < 0) {
+    throw new Error('Age cannot be negative')
+  }
+  
   if (!name || !age) {
     throw 'Arguments not valid'
   } else {
@@ -9,10 +13,7 @@ function createGreeting(name, age) {
   
 }
 
-function getYearOfBirth(age) {
-  if (age < 0) {
-    throw new Error('Age cannot be negative')
-  }
+function getYearOfBirth(age) {}
   return 2019 - age;
 }
 
