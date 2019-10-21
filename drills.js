d636f13dd89d3dab621e5abce9b790223c1ce4cb
Daniel Kent <Kentpercussion@gmail.com>
2019-10-21 13:16:20 -0600

    added DOB

--- drills.js
+++ drills.js
@@ -1,6 +1,7 @@
 'use strict';
 function createGreeting(name, age) {
-  return `Hi, my name is ${name}  and I am ${age}.`;
+  const yearOfBirth = 2019 - age;
+  return `Hi, my name is ${name} and I am ${age}. I was born in ${yearOfBirth}`;
 }
 
 const greeting = createGreeting();
