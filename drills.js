1c71a4b4a9293d989d37ae11858833bf1fb1545d
CyncerelyCrya <cyncerelycyra@gmail.com>
2019-10-21 14:20:01 -0500

    added new function

--- drills.js
+++ drills.js
@@ -1,7 +1,11 @@
 'use strict';
 function createGreeting(name, age) {
-  const yearOfBirth = 2019 - age;
+  
   return `Hi, my name is ${name} and I am ${age}. I was born in ${yearOfBirth}`;
+}
+
+function getYearOfBirth(age) {
+    return  2019 - age;
 }
 
 const greeting = createGreeting();
